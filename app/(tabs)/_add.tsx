8db9da7959da1000183d218f25f9e564e53db74a
Charlie C <work@charle.cc>
2025-06-28 16:23:57 +0800

    remove

--- app/(tabs)/_add.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native';
-
-export default function AddScreen() {
-  return (
-    <View className="flex-1 items-center justify-center bg-[#fffcf6]">
-      <Text className="text-xl text-[#4f4f4f]">Add Screen</Text>
-    </View>
-  );
-}
--- app/(tabs)/track.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native';
-
-export default function TrackScreen() {
-  return (
-    <View className="flex-1 items-center justify-center bg-[#fffcf6]">
-      <Text className="text-xl text-[#4f4f4f]">Track Screen</Text>
-    </View>
-  );
-}
