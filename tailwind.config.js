97b8cdda32ee1ada95ba5afbdacd2e116622759f
Charlie C <work@charle.cc>
2025-06-28 16:03:26 +0800

    initial tabs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./App.{js,ts,tsx}', './components/**/*.{js,ts,tsx}'],
+  content: ['./app/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
 
   presets: [require('nativewind/preset')],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: '#fffcf6',
+        'primary-text': '#4f4f4f',
+        'accent-text': '#707070',
+        'secondary-background': '#ffecbc',
+        'secondary-primary': '#d86a2b',
+      },
+    },
   },
   plugins: [],
 };
