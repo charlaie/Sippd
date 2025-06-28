e4ae8b66892670c700f88663640b0b7b07b21706
Charlie C <github@charle.cc>
2025-06-28 18:28:05 +0800

    Added useFrameworkReady.ts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,10 @@
 import 'react-native-gesture-handler';
 import '../global.css';
-import { Stack } from 'expo-router';
+import { Stack } from 'expo-router'
+import { useFrameworkReady } from '@/hooks/useFrameworkReady';
 
 export default function RootLayout() {
+  useFrameworkReady();
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
