2cea544e00a9ec5c31bed65c2374822477d296e5
Charlie C <github@charle.cc>
2025-06-30 00:23:54 +0800

    Updated package.json

--- metro.config.js
+++ metro.config.js
@@ -3,10 +3,4 @@ const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 
-// Add resolver configuration to handle import.meta properly
-config.resolver = {
-  ...config.resolver,
-  unstable_enablePackageExports: true,
-};
-
 module.exports = withNativeWind(config, { input: './global.css' });
